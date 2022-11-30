2264709de6b251a8b2fe92dde12f7ca6b9d27abb
Berat Günaydın <116026350+BeratG28@users.noreply.github.com>
2022-11-30 17:52:51 +0300

    Add files via upload

---
+++ KLAVYEDEN GİRİNCE DIKDORTGENIN ALANINI HESAPLAYAN PROGRAM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, Alan;
+            
+            Console.Write("Uzun kenarın Ölçüsünü giriniz");
+            a= Convert.ToInt16(Console.ReadLine());
+            Console.Write("Kısa kenarın ölçüsünü giriniz");
+            b= Convert.ToInt16(Console.ReadLine());
+            Alan= a * b; 
+            Console.Write(Alan);
+
+            Console.Read();
+
+        }
+    }
+}
